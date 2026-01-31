f60110388a7dbb66be3eea6050424bf4be3d2f38
davidcllm <davcm24@hotmail.com>
2026-01-30 19:50:09 -0600

    HomeActivity y HomeViewModel

--- app/src/main/java/david/ceballos/demo/scenes/main/home/view/HomeActivity.kt
+++ app/src/main/java/david/ceballos/demo/scenes/main/home/view/HomeActivity.kt
@@ -1,21 +1,42 @@
 package david.ceballos.demo.scenes.main.home.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import david.ceballos.demo.R
+import david.ceballos.demo.databinding.ActivityHomeBinding
+import david.ceballos.demo.scenes.base.BaseActivity
+import david.ceballos.demo.scenes.main.home.viewModel.HomeViewModel
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var viewModel: HomeViewModel
+    private val TAG = this::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        this.enableEdgeToEdge()
+        this.configureActivity()
+    }
+
+    private fun configureActivity() {
+        this.initActivityView()
+        this.setObserver()
+    }
+
+    private fun initActivityView() {
+        this.binding = ActivityHomeBinding.inflate(layoutInflater) //inicializar binding del xml
+        this.setContentView(this.binding.root) //envia la vista del binding
+        this.viewModel = HomeViewModel(this, this) // Inicializa el Home view model
+    }
+
+    private fun setObserver() {
+        this.viewModel.name.observe(this) { userName ->
+            Log.i(TAG, "userName: $userName")
+            this.binding.tvName.text = userName
         }
     }
 }
---
+++ app/src/main/java/david/ceballos/demo/scenes/main/home/viewModel/HomeViewModel.kt
@@ -0,0 +1,35 @@
+package david.ceballos.demo.scenes.main.home.viewModel
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import david.ceballos.demo.scenes.base.BaseActivity
+import david.ceballos.demo.scenes.main.home.model.HomeModel
+
+class HomeViewModel(val context: Context, val activity: BaseActivity): ViewModel() {
+    private val TAG = this::class.java.simpleName
+    private val model = HomeModel()
+
+    // Funcionan como las propiedades estaticas en java
+    // Propiedades que no necesitan ser instanciadas, sino que pertenencen a la misma clase
+    companion object {
+        val PARAM_NAME = "PARAM_NAME"
+
+        fun hello() = print("Hello World")
+    }
+
+    /*
+        Funcion que se ejecuta cuando se inicializa el viewModel
+     */
+    // GetSerializable para pasar parametro de una pantalla anterior a la nueva
+    // Lo intenta convertir en String, si no, se hace nulo
+    init {
+        this.model.name.value = (this.activity.intent.getSerializableExtra(PARAM_NAME) as? String) ?: ""
+    }
+
+    /*
+        Variable que permite obtner el nombre y cambiar su valor desde el model
+     */
+    val name: LiveData<String>
+        get() = this.name
+}
