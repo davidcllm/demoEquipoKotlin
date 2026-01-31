c2c9556e97bf732340990c9e8a2444eab66f7e1f
davidcllm <davcm24@hotmail.com>
2026-01-30 19:18:49 -0600

    Comentario the finish()

--- app/src/main/java/david/ceballos/demo/scenes/splash/router/SplashRouter.kt
+++ app/src/main/java/david/ceballos/demo/scenes/splash/router/SplashRouter.kt
@@ -17,7 +17,7 @@ class SplashRouter (val context: Context, val activity: SplashActivity) {
 
         // Finaliza la actividad actual para no poder retroceder
         // Si no se coloca el usuatio puede retroceder a la actividad anterior
-        this.activity.finish()
+        this.activity.finish() // Esta linea previene que el usuario pueda regresar al splash
     }
 
 }
