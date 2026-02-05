7202eb65150d07fa73e8c30678f0a5fc00eec318
davidcllm <davcm24@hotmail.com>
2026-02-04 20:00:38 -0600

    Vista ListView

--- app/src/main/java/david/ceballos/demo/scenes/home/view/HomeActivity.kt
+++ app/src/main/java/david/ceballos/demo/scenes/home/view/HomeActivity.kt
@@ -3,9 +3,12 @@ package david.ceballos.demo.scenes.home.view
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.Fragment
+import david.ceballos.demo.R
 import david.ceballos.demo.databinding.ActivityHomeBinding
 import david.ceballos.demo.scenes.base.BaseActivity
 import david.ceballos.demo.scenes.home.viewModel.HomeViewModel
+import david.ceballos.demo.scenes.list.ListFragment
 
 class HomeActivity : BaseActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -20,6 +23,8 @@ class HomeActivity : BaseActivity() {
 
     private fun configureActivity() {
         this.initActivityView()
+        this.configurerListeners()
+        this.replaceFragment(ListFragment())
         //this.setObserver()
     }
 
@@ -27,6 +32,25 @@ class HomeActivity : BaseActivity() {
         this.binding = ActivityHomeBinding.inflate(layoutInflater) //inicializar binding del xml
         this.setContentView(this.binding.root) //envia la vista del binding
         this.viewModel = HomeViewModel(this, this) // Inicializa el Home view model
+    }
+
+    private fun initActivity() {
+
+    }
+
+    private fun configurerListeners() {
+        this.binding.bnvHome.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.list_menu -> this.replaceFragment(ListFragment())
+                R.id.profile_menu -> this.replaceFragment(ListFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = this.supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_content, fragment).commit()
     }
 
     /*private fun setObserver() {
