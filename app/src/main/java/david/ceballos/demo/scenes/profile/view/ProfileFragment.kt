a40ca35924dba3ae65406788382ee6bb812694ea
davidcllm <davcm24@hotmail.com>
2026-02-06 20:19:56 -0600

    Inserción de video

--- app/src/main/java/david/ceballos/demo/scenes/profile/view/ProfileFragment.kt
+++ app/src/main/java/david/ceballos/demo/scenes/profile/view/ProfileFragment.kt
@@ -1,11 +1,13 @@
 package david.ceballos.demo.scenes.profile.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import david.ceballos.demo.databinding.FragmentProfileBinding
+import david.ceballos.demo.scenes.help.HelpActivity
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
@@ -16,6 +18,13 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         this.binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        this.binding.cvHelp.setOnClickListener {
+            this.startActivity(
+                Intent(activity, HelpActivity::class.java)
+            )
+        }
+
         return this.binding.root
     }
 
