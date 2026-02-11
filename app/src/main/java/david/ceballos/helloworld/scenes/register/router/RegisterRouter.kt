2b62c4d80ce4f23073e9e44f57a57e30cc1b05d2
davidcllm <davcm24@hotmail.com>
2026-02-11 10:46:19 -0600

    RegisterRouter
    Creo que esto servirá para regresar a la pantalla de login cuando se requiera.

--- app/src/main/java/david/ceballos/helloworld/scenes/register/router/RegisterRouter.kt
+++ app/src/main/java/david/ceballos/helloworld/scenes/register/router/RegisterRouter.kt
@@ -1,4 +1,30 @@
 package david.ceballos.helloworld.scenes.register.router
 
-class RegisterRouter {
+import android.content.Context
+import android.content.Intent
+import david.ceballos.helloworld.scenes.base.BaseActivity
+import david.ceballos.helloworld.scenes.home.view.HomeActivity
+import david.ceballos.helloworld.scenes.home.viewModel.HomeViewModel
+import david.ceballos.helloworld.scenes.main.view.MainActivity
+import david.ceballos.helloworld.scenes.main.viewModel.MainViewModel
+
+class RegisterRouter(val context: Context, val activity: BaseActivity) {
+    private val TAG = this::class.java.simpleName
+
+    // Regresar al login
+    fun routeToMainView(name: String) {
+        //Levanta otra actividad. Mercado libre
+        val intent = Intent(this.context, MainActivity::class.java)
+
+        // PutExtra envia parametros
+        intent.putExtra(MainViewModel.PARAM_NAME, name)
+
+        //starActivity inicia el intent
+        this.context.startActivity(intent)
+
+        // Finaliza la actividad actual para no poder retroceder
+        // Si no se coloca el usuatio puede retroceder a la actividad anterior
+        this.activity.finish() // Finaliza la actividad actual
+    }
+
 }
